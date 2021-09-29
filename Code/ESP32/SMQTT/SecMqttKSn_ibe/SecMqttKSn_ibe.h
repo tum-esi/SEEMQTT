8cf35146652a8b920d1eacc6c3367bddc5cda8c9
Mohammad <mhh.it1986@gmail.com>
2021-09-29 21:21:45 +0200

    update copyrights

--- Code/ESP32/SMQTT/SecMqttKSn_ibe/SecMqttKSn_ibe.h
+++ Code/ESP32/SMQTT/SecMqttKSn_ibe/SecMqttKSn_ibe.h
@@ -1,3 +1,12 @@
+
+/*
+ * Copyright (c) 2021 Hangmao Liu <liuhangmao@hotmail.com>
+ *                    Mohammad Hamad <mohammad.hamad@tum.de>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See COPYING for details.
+ *
+ */
 /*
   This library is a wrapper from PubSubClient library
   mbedtls library is used to do aes encryption and rsa signature
