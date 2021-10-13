f2213c270035d32513a8e384a8515767afa3ed0a
Mohammad <mhh.it1986@gmail.com>
2021-10-13 18:51:01 +0200

    update db

--- Code/RPI/SMQTT/ks_ibe_sss/db.h
+++ Code/RPI/SMQTT/ks_ibe_sss/db.h
@@ -7,32 +7,32 @@
 #include <unistd.h>
 #include "typ.h"
 
-#define MAXDBROW 10
-#define SHARPATH "SHARDB"
-
+#define MAXDBROW 10 
+#define SHARPATH "../../SHARDB"
 
 typedef struct list {
 unsigned char hiot[HASH_LEN];
-unsigned char htpk[HASH_LEN];
-unsigned char msymk[BLOCK_SIZE] ;
-unsigned char shar[SHAR_LEN];
-int isuded ;
-	} LIST;
+unsigned char htpk[HASH_LEN]; 
+unsigned char msymk[BLOCK_SIZE] ; 
+unsigned char shar[SHAR_LEN];  
+int isuded ; 
+	} LIST; 
+	
+
+LIST db[MAXDBROW]; 
 
 
-LIST db[MAXDBROW];
-
-
-void SaveShar(int id, LIST lst);
-void Initlst (LIST l);
-void Initdb();
-void Printlst(LIST l ) ;
-void Printdb();
+void SaveShar(int id, LIST lst); 
+void Initlst (LIST l); 
+void Initdb(); 
+void Printlst(LIST l ) ; 
+void Printdb(); 
 int GetShare(const unsigned  char * iotid, const unsigned char * tpkid, unsigned char * share );
-int GetSymK(const unsigned  char * iotid,  unsigned char * symk );
-int Getindex (const unsigned  char * iotid);
-int AddsymK(const unsigned  char * iotid, const unsigned char * symk);
+int GetSymK(const unsigned  char * iotid,  unsigned char * symk ); 
+int Getindex (const unsigned  char * iotid); 
+int AddsymK(const unsigned  char * iotid, const unsigned char * symk); 
 int Addshare (const unsigned  char * iotid, const unsigned char * tpkid, const unsigned char * share );
-int AddRow(const unsigned  char * iotid,  const unsigned char * symk, const unsigned char * tpkid, const unsigned char * share);
+int AddRow(const unsigned  char * iotid,  const unsigned char * symk, const unsigned char * tpkid, const unsigned char * share); 
 
 #endif
+
