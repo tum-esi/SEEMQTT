a2385f85f78c8c3e4503dc1c840f1ffa68cc412d
Mohammad <mhh.it1986@gmail.com>
2022-01-06 21:11:12 +0100

    Delete mosquitto_pw_start.sh

--- ESP32/Clear-MQTT/mosquitto_pw_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Stop current mosquitto service and clear configuration file
-service mosquitto stop
-killall mosquitto
-rm -f /etc/mosquitto/conf.d/*.conf
-
-# Start mosquitto broker with new configuration file
-mosquitto -c mosquitto_pw.conf
