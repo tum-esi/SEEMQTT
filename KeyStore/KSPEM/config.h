97f6a52760c5872c81ae76e290fddea268e4a43b
Mohammad <mhh.it1986@gmail.com>
2022-01-06 21:01:14 +0100

    Create config.h

---
+++ KeyStore/KSPEM/config.h
@@ -0,0 +1,16 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define DBG 
+#define SHARPATH "../SHARDB"
+
+#define KS_NUM 3
+
+struct KS_INFO {
+int id ; 
+char ip [16]; 
+int port}; 
+
+struct KS_INFO  kslist[KS_NUM]; 
+
+#endif
